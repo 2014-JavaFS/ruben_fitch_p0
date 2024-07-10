406c3e30f6802fd66cf90a0fc648f4473382d768
rubensamf <ruben146@revature.net>
2024-07-10 10:00:17 -0500

    Added packages

---
+++ src/main/java/com/revature/cra/CourseRegistrationRunner.java
@@ -0,0 +1,14 @@
+/**
+ * Project 0 - Course Registration Application
+ * Author: Ruben Fitch
+ * Due Date: 07/17/2024
+ * Description: Allows users to manage their classes
+ */
+package com.revature.cra;
+
+// FrontController
+public class CourseRegistrationRunner {
+    public static void main(String[] args) {
+        System.out.println("Course Registration Application Running....");
+    }
+}
