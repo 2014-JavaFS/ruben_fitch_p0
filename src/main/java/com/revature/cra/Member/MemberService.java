f6f0e1c48ad6655d4b413df0580888e33d721500
rubensamf <ruben146@revature.net>
2024-07-10 11:03:14 -0500

    Added DataNotFoundException Finished MemberService

--- src/main/java/com/revature/cra/Member/MemberService.java
+++ src/main/java/com/revature/cra/Member/MemberService.java
@@ -2,6 +2,7 @@ package com.revature.cra.Member;
 
 import com.revature.cra.util.Interfaces.Serviceable;
 import com.revature.cra.util.exceptions.InvalidInputException;
+import com.revature.cra.util.exceptions.DataNotFoundException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,4 +30,41 @@ public class MemberService implements Serviceable<Member> {
         }
         return null;
     }
+
+
+    /**
+     * Searches database for information where the memberId and password must be equal to
+     * a row within our database
+     *
+     * @param memberId - int
+     * @param password - String
+     * @return - Member object, null if not found
+     */
+    public Member findByMemberIdAndPassword(int memberId, String password){
+        for (Member member: memberList){
+            if (member.getMemberId() == memberId && member.getPassword().equals(password)){
+                return member;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Find the member with updatedMember's ID and replace it
+     * Otherwise, throw a DataNotFoundException
+     *
+     * @param updatedMember - an existing member with updated information
+     * @throws DataNotFoundException - member not found within database
+     */
+    public void update(Member updatedMember) throws DataNotFoundException {
+        for (int i = 0; i < memberList.size(); i++) {
+            if (memberList.get(i).getMemberId() == updatedMember.getMemberId()){
+                memberList.set(i, updatedMember);
+                return;
+            }
+        }
+        throw new DataNotFoundException("Member with ID not within Database");
+    }
+
+
 }
